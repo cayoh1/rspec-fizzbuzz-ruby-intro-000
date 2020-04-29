92a3f2927d7d14b8ef4ee5095d27bbcb13b0ba4e
Christine Ayoh <christineayoh9@gmail.com>
2020-04-29 03:53:55 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,11 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizbuzz(x)
-  if x % 3 == 0:
+  if x % 3 == 0
     puts "Fizz"
-  elsif x % 5 == 0:
+  elsif x % 5 == 0
     puts "Buzz"
-  elsif x % 5 == 0 && x%3 == 0:
+  elsif x % 5 == 0 && x%3 == 0
     puts "FizzBuzz"
   else
     puts nil

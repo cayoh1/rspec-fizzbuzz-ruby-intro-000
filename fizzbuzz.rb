9775a13376b243a4342fddfc12669b0d02a6f830
Christine Ayoh <christineayoh9@gmail.com>
2020-04-29 03:58:49 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,15 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(x)
   if x % 3 == 0
+    puts "Fizz"
     return "Fizz"
   elsif x % 5 == 0
+    puts "Buzz"
     return "Buzz"
   elsif (x % 5 == 0) && (x % 3 == 0)
+    puts "FizzBuzz"
     return "FizzBuzz"
   else
+    puts "NADA"
     return nil
 end
